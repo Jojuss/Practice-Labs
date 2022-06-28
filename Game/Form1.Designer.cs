cafda4c3488ac51d7416cdc48c40887a5a50de6d
Jojuss <42657391+Jojuss@users.noreply.github.com>
2022-06-28 23:39:50 +0400

    Add files via upload

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.gameoverlabel = new System.Windows.Forms.Label();
+            this.win_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 760);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.scorelabel);
+            this.panel1.Location = new System.Drawing.Point(457, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(129, 760);
+            this.panel1.TabIndex = 1;
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Font = new System.Drawing.Font("Montserrat Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scorelabel.ForeColor = System.Drawing.Color.White;
+            this.scorelabel.Location = new System.Drawing.Point(15, 252);
+            this.scorelabel.MaximumSize = new System.Drawing.Size(100, 50);
+            this.scorelabel.MinimumSize = new System.Drawing.Size(100, 50);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(100, 50);
+            this.scorelabel.TabIndex = 0;
+            this.scorelabel.Text = "Score: ";
+            // 
+            // gameoverlabel
+            // 
+            this.gameoverlabel.AutoSize = true;
+            this.gameoverlabel.BackColor = System.Drawing.Color.Transparent;
+            this.gameoverlabel.Font = new System.Drawing.Font("Montserrat Light", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gameoverlabel.ForeColor = System.Drawing.Color.Black;
+            this.gameoverlabel.Location = new System.Drawing.Point(77, 252);
+            this.gameoverlabel.MaximumSize = new System.Drawing.Size(300, 200);
+            this.gameoverlabel.MinimumSize = new System.Drawing.Size(300, 200);
+            this.gameoverlabel.Name = "gameoverlabel";
+            this.gameoverlabel.Size = new System.Drawing.Size(300, 200);
+            this.gameoverlabel.TabIndex = 1;
+            this.gameoverlabel.Text = "Game over! Press R to restart. ";
+            this.gameoverlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gameoverlabel.Click += new System.EventHandler(this.gameoverlabel_Click);
+            // 
+            // win_label
+            // 
+            this.win_label.AutoSize = true;
+            this.win_label.BackColor = System.Drawing.Color.Transparent;
+            this.win_label.Font = new System.Drawing.Font("Montserrat Light", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.win_label.ForeColor = System.Drawing.Color.Black;
+            this.win_label.Location = new System.Drawing.Point(77, 252);
+            this.win_label.MaximumSize = new System.Drawing.Size(300, 200);
+            this.win_label.MinimumSize = new System.Drawing.Size(300, 200);
+            this.win_label.Name = "win_label";
+            this.win_label.Size = new System.Drawing.Size(300, 200);
+            this.win_label.TabIndex = 2;
+            this.win_label.Text = "You win! Press R to restart. ";
+            this.win_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 762);
+            this.Controls.Add(this.win_label);
+            this.Controls.Add(this.gameoverlabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Label gameoverlabel;
+        private System.Windows.Forms.Label win_label;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        const int WIDTH = 5;
+        const int HEIGHT = 10;
+
+        Rectangle[,] blocks;
+        int player_x;
+        int player_y;
+        int player_speed;
+
+        int ball_x;
+        int ball_y;
+        int[] ball_speed = new int[] { 4, -4 };
+
+        int score = 0;
+
+        bool gameover = false;
+        bool game_win = false;
+        
+
+        Bitmap bm;
+        Graphics g;
+        Timer movementTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameoverlabel.Hide();
+            win_label.Hide();
+
+            blocks = new Rectangle[WIDTH, HEIGHT];
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++) blocks[i, j] = new Rectangle(i * (pictureBox1.Width / WIDTH), j * 20, pictureBox1.Width / WIDTH - 2, 18);
+            }
+            player_x = 200;
+            player_y = 700;
+            ball_x = 200;
+            ball_y = 650;
+
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bm);
+            this.KeyDown += new KeyEventHandler(processInputDown);
+            this.KeyUp += new KeyEventHandler(processInputUp);
+
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(timerUpdate);
+            timer1.Enabled = true;
+
+            movementTimer.Interval = 10;
+            movementTimer.Tick += new EventHandler(movementTick);
+        }
+
+
+
+        private void DrawEverything()
+        {
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bm);
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++) g.FillRectangle(Brushes.Gray, blocks[i, j]);
+            }
+            g.FillRectangle(Brushes.Black, new Rectangle(player_x, player_y, 100, 20));
+            g.FillEllipse(Brushes.Black, new Rectangle(ball_x, ball_y, 20, 20));
+            pictureBox1.Image = bm;
+            scorelabel.Text = "Score: " + score;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkCollisions()
+        {
+            //Walls
+            if (ball_x + ball_speed[0] + 20 > pictureBox1.Width || ball_x + ball_speed[0] < 0) ball_speed[0] *= -1;
+            if (ball_y + ball_speed[1] < 0) ball_speed[1] *= -1;
+            if (ball_y + ball_speed[1] > 740) gameover = true;
+
+            //Player
+            if ((player_y - ball_y) < 20 && ball_x + 10 < player_x + 100 && ball_x + 10 > player_x) ball_speed[1] *= -1;
+
+            //Blocks
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    if (blocks[i, j] != null && ball_x + 10 > blocks[i, j].X && ball_x + 10 < blocks[i, j].X + blocks[i, j].Width && Math.Abs(ball_y - blocks[i, j].Y) < 10)
+                    {
+                        blocks[i, j] = Rectangle.Empty;
+                        ball_speed[1] *= -1;
+                        score += 50;
+                    }
+                }
+            }
+
+            ball_x += ball_speed[0];
+            ball_y += ball_speed[1];
+        }
+
+        private void checkWin()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++) if (blocks[i, j] != Rectangle.Empty) return;
+            }
+            game_win = true;
+        }
+
+        private void timerUpdate(object sender, EventArgs e)
+        {
+            if (!gameover && !game_win)
+            {
+                checkCollisions();
+                checkWin();
+                DrawEverything();
+            }
+            else if (gameover)
+            {
+                gameoverlabel.Show();
+            }
+            else
+            {
+                win_label.Show();
+            }
+        }
+
+        private void movementTick(object sender, EventArgs e) 
+        {
+            if (!gameover)
+            {
+                if (!(player_x + player_speed + 100 > pictureBox1.Width) && !(player_x + player_speed < 0))
+                    player_x += player_speed;
+                DrawEverything();
+            }
+        }
+        
+        private void processInputDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                player_speed = -5;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                player_speed = +5;
+            }
+
+            if ((gameover || game_win) && e.KeyCode == Keys.R)
+            {
+                ball_x = 200;
+                ball_y = 650;
+                score = 0;
+                for (int i = 0; i < WIDTH; i++)
+                {
+                    for (int j = 0; j < HEIGHT; j++) blocks[i, j] = new Rectangle(i * (pictureBox1.Width / WIDTH), j * 20, pictureBox1.Width / WIDTH - 2, 18);
+                }
+                gameover = false;
+                game_win = false;
+                gameoverlabel.Hide();
+                win_label.Hide();
+                ball_speed = new int[] { 4, -4 };
+            }
+
+            UpdateTimer();
+            DrawEverything();
+        }
+
+        private void processInputUp(object sender, KeyEventArgs e)
+        {
+            player_speed = 0;
+            UpdateTimer();
+            DrawEverything();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            DrawEverything();
+        }
+
+        private void UpdateTimer()
+        {
+            movementTimer.Enabled = player_speed != 0;
+        }
+
+        private void gameoverlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
